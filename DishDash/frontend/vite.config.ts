14ece09f83e6f5cb3057b11f56fb3a581d2e47bb
Mariia Zhytnikova <allusio@gmail.com>
2026-01-31 14:23:03 +0200

    add base to vite.config

--- DishDash/frontend/vite.config.ts
+++ DishDash/frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  // base: "/DishDash/",
+  base: "/DishDash/",
   plugins: [react()],
   server: {
     proxy: {
